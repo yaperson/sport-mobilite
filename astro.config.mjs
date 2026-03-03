58ae30137705958965be4626a37f43cfe1f95fd5
Yaperson <yanis.legrand.1@gmail.com>
2026-03-03 16:49:15 +0100

    ajout de la tombola

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,16 @@ import sitemap from '@astrojs/sitemap';
 
 import robotsTxt from 'astro-robots-txt';
 
+import playformCompress from '@playform/compress';
+
 const url = 'https://sport-mobilite.fr';
 // https://astro.build/config
 export default defineConfig({
   site: url,
   trailingSlash: 'always',
-  integrations: [
-    sitemap({
-      filter: (page) => 
-        page !== `${url}/admin/` &&
-        page !== `${url}/legal/`,
-    }), 
-    robotsTxt(),
-  ]
+  integrations: [sitemap({
+    filter: (page) => 
+      page !== `${url}/admin/` &&
+      page !== `${url}/legal/`,
+  }), robotsTxt(), playformCompress()]
 });
